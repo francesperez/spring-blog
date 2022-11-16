0943dac926ff7d1da851cf633b8478c12857cc81
francesperez <frances.perez141@gmail.com>
2022-11-16 14:42:18 -0600

    views lecture part 2

--- src/main/java/com/codeup/springblog/controllers/CoffeeController.java
+++ src/main/java/com/codeup/springblog/controllers/CoffeeController.java
@@ -1,8 +1,12 @@
 package com.codeup.springblog.controllers;
 
+import com.codeup.springblog.models.Coffee;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/coffee")
@@ -27,7 +31,12 @@ public class CoffeeController {
 // information.
     @GetMapping("/{roast}")
     public String roast(@PathVariable String roast, Model model){
-        model.addAttribute("roast", roast);
+        Coffee selection = new Coffee(roast, "Cool Beans");
+        Coffee selection2 = new Coffee(roast, "Glory Groovy");
+        selection.setOrigin("Ethiopian");
+        selection2.setOrigin("Vietnam");
+        List<Coffee> selections = new ArrayList<>(List.of(selection, selection2));
+        model.addAttribute("selections", selections);
         return "coffee";
     }
 
